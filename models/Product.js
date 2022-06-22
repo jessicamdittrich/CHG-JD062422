e60d198c997c61dc6904c69a0370d78fd57a5630
Jessica Dittrich <104876367+jessicamdittrich@users.noreply.github.com>
2022-06-22 16:19:07 -0700

    Set up columns for Category, Product, Tag, and ProductTag

--- models/Product.js
+++ models/Product.js
@@ -21,12 +21,19 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
   },
   {
